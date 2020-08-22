d60718da8e40e3c4b8b18a4501d1749696bffde7
Edgar Siqueira Egidio <edgarsiqueiraegidio@Edgars-MacBook-Pro.local>
2020-08-22 15:05:35 -0300

    finalização header Wrapper

--- src/screens/Wallet/index.js
+++ src/screens/Wallet/index.js
@@ -1,6 +1,56 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Feather, MaterialCommunityIcons, FontAwesome } from '@expo/vector-icons';
+
+import {
+  Wrapper,
+  Header,
+  HedaerContainer,
+  Title,
+  BalanceContainer,
+  Value,
+  Bold,
+  EyeButtom,
+  Info,
+  Actions,
+  Action,
+  ActionLabel,
+} from './styles'
 
 export default function Wallet() {
-  return <Text>Wallet</Text> ;
+  return (
+    <Wrapper>
+      <Header colors={['#52e78c', '#1ab56e']}>
+
+        <HedaerContainer>
+          <Title>Saldo PicPay</Title>
+
+          <BalanceContainer>
+            <Value>
+              R$ <Bold>0,00</Bold>
+            </Value>
+
+            <EyeButtom >
+              <Feather name="eye" size={28} color="#FFF" />
+            </EyeButtom>
+          </BalanceContainer>
+
+          <Info>
+            Seu saldo está rendendo 100% do CDI
+          </Info>
+
+          <Actions>
+            <Action>
+              <MaterialCommunityIcons name="cash-multiple" size={26} color="#FFF" />
+              <ActionLabel>Adicionar</ActionLabel>
+            </Action>
+            <Action>
+              <FontAwesome name="bank" size={20} color="#FFF" />
+              <ActionLabel>Retirar</ActionLabel>
+            </Action>
+          </Actions>
+
+        </HedaerContainer>
+      </Header>
+    </Wrapper>
+  );
 }
