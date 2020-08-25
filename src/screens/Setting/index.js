1eb11e9e49a03d56350bc5aa0e1abd9b9ad3879e
Edgar Siqueira Egidio <edgarsiqueiraegidio@Edgars-MacBook-Pro.local>
2020-08-25 15:40:00 -0300

    construção screen setting

--- src/screens/Setting/index.js
+++ src/screens/Setting/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-// import { Container } from './styles';
+import {
+  Wrapper,
+  Header,
+  HedaerContainer,
+  HeaderText,
+  CardProfile,
+  Avatar,
+  LoginPicPay,
+  NamePicPay,
+  CardViewProfile,
+  ViewProfile,
+} from './styles';
+
+import img9 from '../../images/09.png'
 
 export default function Setting() {
   return (
-    <Text>Teste</Text>
+    <Wrapper>
+      <Header>
+        <HedaerContainer>
+          <HeaderText>Ajustes</HeaderText>
+        </HedaerContainer>
+      </Header>
+
+      <CardProfile>
+        <Avatar source={img9}/>
+        <LoginPicPay>@edgar.siqueira.egidio</LoginPicPay>
+        <NamePicPay>Edgar Siqueira Egidio</NamePicPay>
+
+        <CardViewProfile>
+          <ViewProfile>Ver meu perfil</ViewProfile>
+          <MaterialIcons name="navigate-next" size={28} color="#10c86e" />
+        </CardViewProfile>
+
+      </CardProfile>
+    </Wrapper>
   );
 }
---
+++ src/screens/Setting/styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components/native';
+
+export const Wrapper = styled.View`
+  background: #000;
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  background: #39464E;
+  height: 100px;
+`;
+
+export const HedaerContainer = styled.SafeAreaView`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HeaderText = styled.Text`
+  color: #FFF;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const CardProfile = styled.View`
+  align-items: center;
+  justify-content: center;
+  padding: 25px 0;
+`;
+
+export const Avatar = styled.Image``;
+
+export const LoginPicPay = styled.Text`
+  color: #FFF;
+  font-size: 22px;
+  font-weight: bold;
+  margin-top: 15px;
+`;
+
+export const NamePicPay = styled.Text`
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 16px;
+  margin-top: 5px;
+`;
+
+export const CardViewProfile = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 5px;
+`;
+
+export const ViewProfile = styled.Text`
+  color:#10c86e;
+  font-weight: bold;
+`;
